984003bae9c2a3764421bb471b39c6620ae36f9b
Diogo Salazar <dsalazar83@gmail.com>
2020-03-07 14:15:46 -0500

    commit

--- anagram/src/java/com/dsalazar/anagram/AnagramServer.java
+++ anagram/src/java/com/dsalazar/anagram/AnagramServer.java
@@ -1,11 +1,12 @@
 package com.dsalazar.anagram;
 
 
+import java.awt.PageAttributes.MediaType;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
